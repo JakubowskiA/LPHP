0abe8813adbee927dcb6347b083ae92a833fdd37
JakubowskiA <arieljakubowski@gmail.com>
2019-12-17 17:38:30 -0500

    second file

--- php-classes.php
+++ php-classes.php
@@ -1,1 +1,38 @@
-test
+<?php
+class Pet{
+    var $name;
+    var $age;
+    var $species;
+
+
+    // $pet1 = new Pet;
+
+    // $pet1->name = “Baby”;
+    // $pet1->age = 7;
+    // $pet1->species = “Dog”;
+
+    function __construct( $petName, $petAge, $petSpecies){ 
+        $this->name = $petName;
+        $this->age = $petAge;
+        $this->species = $petSpecies;
+    }
+
+    function happyBirthday(){
+        $this->age++;
+        echo "Happy Birthday, $this->name!<br>";
+        echo "You are now $this->age years old!<br>";
+    }
+
+}
+
+$pet1 = new Pet("Baby", 7, "Dog");
+echo "$pet1->name<br>";
+echo "$pet1->age<br>";
+
+$pet1->happyBirthday()
+
+  
+
+
+
+?>
